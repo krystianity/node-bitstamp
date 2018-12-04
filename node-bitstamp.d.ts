17a831a7fedd8d12dca3582dcc254a4a27c42755
Ami Benson <ami.benson@k1liquidity.com>
2018-12-05 00:14:56 +0200

    add typescript .d file

---
+++ node-bitstamp.d.ts
@@ -0,0 +1,86 @@
+declare module 'node-bitstamp' {
+  export enum BitstampTradeType {
+    buy = '0',
+    sell = '1',
+  }
+  export interface IBitstampOptions {
+    key: string
+    secret: string
+    clientId: string
+    timeout: number
+    rateLimit: boolean
+  }
+
+  export interface IBitstampOrderbook {
+    timestamp: Date
+    bids: string[][]
+    asks: string[][]
+  }
+
+  export interface IBitstampLimitOrder {
+    id: string
+    datetime: Date
+    type: BitstampTradeType
+    price: number
+    amount: number
+  }
+
+  export interface IBitstampLimitOrderInvalid {
+    status: number
+    reason: string
+  }
+
+  export interface BalanceResponseInvalid {
+    error: string // error / OK
+    reason: string // API key not found
+    code: string // API0001
+  }
+
+  interface BitstampNodeRespose {
+    status: number
+    headers: {}
+    body: string
+  }
+
+  export class Bitstamp {
+    constructor(options: IBitstampOptions)
+
+    ticker(currency?: CURRENCY): Promise<any>
+    orderBook(currency?: CURRENCY): Promise<{ status: number; headers: {}; body: IBitstampOrderbook }>
+
+    balance(): Promise<BalanceResponseInvalid | any>
+
+    // amount is quantity, price is per unit
+    buyLimitOrder(
+      amount: number,
+      price: number,
+      currency: CURRENCY,
+      limit_price?: number,
+      daily_order?: boolean,
+    ): Promise<BitstampNodeRespose> // | IBitstampLimitOrderInvalid>
+
+    // amount is quantity, price is per unit
+    sellLimitOrder(
+      amount: number,
+      price: number,
+      currency: CURRENCY,
+      limit_price?: number,
+      daily_order?: boolean,
+    ): Promise<BitstampNodeRespose> //  | IBitstampLimitOrderInvalid>
+
+    transactions(currency?: CURRENCY, time?: string): Promise<any>
+  }
+  export enum CURRENCY {
+    BTC_EUR,
+    EUR_USD,
+    XRP_USD,
+    XRP_EUR,
+    XRP_BTC,
+    LTC_USD,
+    LTC_EUR,
+    LTC_BTC,
+    ETH_USD,
+    ETH_EUR,
+    ETH_BTC,
+  }
+}
